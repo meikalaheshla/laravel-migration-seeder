03ecb5648234caa34237dd0704dcbc552b15ebc8
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-20 23:53:26 +0200

    corrected miss script finished task

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -12,14 +12,14 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 0;i < 10; $i++){
+        for($i = 0; $i < 10; $i++) {
                 $new_train = new Train();
             
                 $new_train->azienda = $faker->company();
                 $new_train->stazione_di_partenza = $faker->streetName();
                 $new_train->stazione_di_arrivo = $faker->streetName();
-                $new_train->orario_di_partenza = $faker->time();
-                $new_train->orario_di_partenza = $faker->time();
+                $new_train->orario_di_partenza = $faker->dateTimeAD('now');
+                $new_train->orario_di_arrivo = $faker->dateTimeAD('now');
                 $new_train->codice_treno = $faker->numberBetween(0, 1000);
                 $new_train->numero_carrozze = $faker->numberBetween(0, 20);
                 $new_train->in_orario = $faker->boolean();
