c426b45cb4a7e81cb90fa53261d9f1d5ae924793
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-20 23:09:13 +0200

    setted faker and faker methods

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainTableSeeder extends Seeder
 {
@@ -9,8 +10,23 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0;i < 10; $i++){
+                $new_train = new Train();
+            
+                $new_train->azienda = $faker->company();
+                $new_train->stazione_di_partenza = $faker->streetName();
+                $new_train->stazione_di_arrivo = $faker->streetName();
+                $new_train->orario_di_partenza = $faker->time();
+                $new_train->orario_di_partenza = $faker->time();
+                $new_train->codice_treno = $faker->numberBetween(0, 1000);
+                $new_train->numero_carrozze = $faker->numberBetween(0, 20);
+                $new_train->in_orario = $faker->boolean();
+                $new_train->cancellato = $faker->boolean();
+                
+                $new_train->save();
+            
+        }
     }
 }
